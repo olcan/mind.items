57ee945bb93c48607a1364bc91d9571a5a1586b4
Olcan <olcans@gmail.com>
2021-10-22 01:45:15 -0700

    simplified updater.js

--- updater.js
+++ updater.js
@@ -1,8 +1,4 @@
-function _install() {
-  // check github token, prompt if needed
-}
 function _init() {
-  // check github token, prompt if needed
   // get last commit sha for each installed item (same as _update)
   // simply replace updated items, no confirmation
   // just log the update
