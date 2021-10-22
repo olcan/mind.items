c8eae3c3fcac2eb06d2a04fda3b16f10b09eaedb
olcan <olcans@gmail.com>
2021-10-22 12:11:32 -0700

    listen for github_webhooks

--- updater.js
+++ updater.js
@@ -1,16 +1,26 @@
-function _welcome() { update() }
-
-async function update() {
+function _welcome() {
   _items().forEach(item => { 
     if (item.attr) update_item(item) 
   })
+
+  // listen for github webhooks through firebase
+  console.log(`listening for github_webhooks ...`)
+  firebase
+  .firestore()
+  .collection("github_webhooks")
+  .where("time", ">", Date.now())
+  .onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type != "added") return; // new documents only
+      console.log("received github_webhooks", change.doc.data());
+    });
+  });
 }
 
 async function update_item(item) {
+  console.log(`updating item ${item.name} ...`)
   // get last commit sha
   // get latest content for item, including any embeds
   // replace updated item
   // log the update
 }
-
-// TODO: do more..
