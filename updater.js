72f8fd864fb03c894b110ef6408c6d6bbd65284a
olcan <olcans@gmail.com>
2021-10-22 12:55:13 -0700

    filter by owner/repo/branch, cleanup and prettier formatter

--- updater.js
+++ updater.js
@@ -1,38 +1,55 @@
 function _on_welcome() {
-  _items().forEach(item => { 
-    if (item.attr) update_item(item) 
-  })
+  _items().forEach((item) => {
+    if (item.attr) update_item(item);
+  });
 
   // listen for github webhooks through firebase
-  console.log(`listening for github_webhooks ...`)
+  console.log(`listening for github_webhooks ...`);
   firebase
-  .firestore()
-  .collection("github_webhooks")
-  .where("time", ">", Date.now())
-  .onSnapshot((snapshot) => {
-    snapshot.docChanges().forEach((change) => {
-      if (change.type != "added") return; // new documents only
-      console.log("received github_webhooks", change.doc.data());
-      // scan commits for modifications to item source paths ...
-      const commits = change.doc.data().body?.commits ?? []
-      commits.forEach(commit => {
-        if (!commit.modified?.length) return; // modified paths only
-        _items().forEach(item => { 
-          // TODO: filter by attr.owner/repo/branch!
-          // TODO: test change to embed
-          if (!item.attr?.path) return // not an installed item or missing path          
-          let paths = [item.attr.path, ...(item.attr.embeds?.map(e => e.path) ?? [])]
-          paths = paths.map(path=>path.replace(/^\//, "")) // remove leading slashes
-          if (paths.some(path=>commit.modified.includes(path)))
-            update_item(item)
-        })
-      })
+    .firestore()
+    .collection("github_webhooks")
+    .where("time", ">", Date.now())
+    .onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type != "added") return; // new documents only
+        const body = change.doc.data().body;
+        if (!body?.refs?.startsWith("refs/heads/")) return; // branch updates only
+        console.log("received github_webhooks", change.doc.data());
+
+        const branch = body.refs.replace("refs/heads/", "");
+        const repo = body.repository.name;
+        const owner = body.repository.owner.login;
+        const commits = (body.commits ?? []).filter((c) => c.modified?.length);
+        if (commits.length == 0) return; // no commits w/ modifications
+
+        // scan items for installed items w/ modified paths
+        _items().forEach((item) => {
+          if (!item.attr) return; // item not installed
+          if (
+            item.attr.owner != owner ||
+            item.attr.repo != repo ||
+            item.attr.branch != branch
+          )
+            return; // item not from modified repo/branch
+          // calculate item paths, including any embeds, removing slash prefixes
+          let paths = [
+            item.attr.path,
+            ...(item.attr.embeds?.map((e) => e.path) ?? []),
+          ].map((path) => path.replace(/^\//, ""));
+          // update item if any paths were modified in any commits
+          if (
+            commits.some((commit) =>
+              paths.some((path) => commit.modified.includes(path))
+            )
+          )
+            update_item(item);
+        });
+      });
     });
-  });
 }
 
 async function update_item(item) {
-  console.log(`updating item ${item.name} ..`)
+  console.log(`updating item ${item.name} ..`);
   // get last commit sha
   // get latest content for item, including any embeds
   // replace updated item
