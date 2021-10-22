63b0fd2495886bdebaba3ee5b0ab2c141013f441
Olcan <olcans@gmail.com>
2021-10-22 01:41:38 -0700

    started implementing updater.js

--- updater.js
+++ updater.js
@@ -1,3 +1,10 @@
+function _install() {
+  // check github token, prompt if needed
+}
 function _init() {
+  // check github token, prompt if needed
+  // get last commit sha for each installed item (same as _update)
+  // simply replace updated items, no confirmation
+  // just log the update
   alert('hello world')
 }
