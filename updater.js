d893c9293c5a289a3f180eec83d426840f93b959
Olcan <olcans@gmail.com>
2021-10-24 01:23:18 -0700

    #updater:updater.js edited in mind.page

--- updater.js
+++ updater.js
@@ -2,7 +2,7 @@
 // ensures page is ready to display modals, e.g. for token prompts
 // also allows existing items to initialize before being updated
 function _on_welcome() {
-  init_updater() // test yet again!
+  init_updater() // test yet again hello!
 }
 
 let modified_ids = []
