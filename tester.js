c8e4f6dec041527d5e67115e008de6e2792ad5c3
Olcan <olcans@gmail.com>
2021-10-25 00:07:42 -0700

    fix undefined variable start

--- tester.js
+++ tester.js
@@ -8,8 +8,8 @@ function _on_item_change(id, label, prev_label, deleted, remote, dependency) {
   // evaluate any functions _test|_test_*() defined on item
   const tests = ['_test', ...(item.text.match(/_test_\w+/g) ?? [])]
   tests.forEach(test => {
-    start = Date.now()
     try {
+      const start = Date.now()
       item.eval(`if (typeof ${test} == 'function') _test()`)
       _this.log(`${test} passed on ${item.name} in ${Date.now() - start}ms`)
     } catch (e) {
