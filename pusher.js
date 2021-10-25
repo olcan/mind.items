59e6f99a17fe161a1d8c3a73960958e0a5f986c7
Olcan <olcans@gmail.com>
2021-10-25 01:35:57 -0700

    added todos to pusher.js

---
+++ pusher.js
@@ -0,0 +1,3 @@
+// TODO: implement pusher based on (simplified) #github, refactoring common code later into another item, perhaps #github.
+// TODO: would be nice if pusher can handle side-push more gracefully, live-tracking changes across devices/tabs like regular pushes
+// TODO: can define commands like /push, /pull, etc in this file!
