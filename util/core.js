e72b67da79490380d67f2af918ce21168cde786f
olcan <olcans@gmail.com>
2021-10-28 12:47:22 -0700

    test change to util/core.js

--- util/core.js
+++ util/core.js
@@ -1,5 +1,6 @@
 // TODO: split core into even smaller items, starting w/ types.
 //       this allows container items that can document w/ examples.
+//       test
 
 // TODO: actually test/benchmark stuff below ...
 function _test() {}
