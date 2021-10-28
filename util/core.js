fb5bae87e82d0fbcecc8edf64dc68cf7cb239185
olcan <olcans@gmail.com>
2021-10-28 12:46:17 -0700

    core todos

--- util/core.js
+++ util/core.js
@@ -1,3 +1,6 @@
+// TODO: split core into even smaller items, starting w/ types.
+//       this allows container items that can document w/ examples.
+
 // TODO: actually test/benchmark stuff below ...
 function _test() {}
 function _test_something() {}
