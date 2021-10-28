bf4242541381b39230c23cc57a74fd89f85f90e4
olcan <olcans@gmail.com>
2021-10-28 14:38:40 -0700

    rewrote types to simplify and standardize

--- util/core/types.js
+++ util/core/types.js
@@ -3,31 +3,31 @@ function _test_something() {}
 function _benchmark() {}
 function _benchmark_something() {}
 
-// types
-const inf = Infinity
-const defined = x => x !== undefined
-// NOTE: for null/undefined together, use ?? ("nullish coalescing")
-const if_defined = (x, y) => (x !== undefined ? x : y)
-const if_finite = (x, y) => (isFinite(x) ? x : y)
+// TODO: redefine all using lowercase notation and start
+//       testing/benchmarking/documenting!
 
-const { isArray } = Array
-const { isInteger } = Number
-const isNumber = x => typeof x == 'number'
-const isNumeric = x =>
-  isNumber(x) ||
+const inf = Infinity
+const defined = x =>
+  isString(x) ? eval('typeof ' + x) !== 'undefined' : x !== undefined
+
+const is_integer = Number.isInteger
+const is_number = x => typeof x == 'number'
+const is_numeric = x =>
+  is_number(x) ||
   // from https://stackoverflow.com/a/175787 ...
   (typeof x == 'string' && !isNaN(x) && !isNaN(parseFloat(x)))
-const isString = x => typeof x == 'string'
-const isArrayLike = o => every(keys(o), (k, j) => k == j)
-const isObject = x => typeof x == 'object'
-const isFunction = x => typeof x == 'function'
-let isRandom = x => false // to be redefined later
-const isBoolean = x => typeof x == 'boolean'
-// simple Enum that extends Array w/ properties
-const Enum = (...nK) =>
-  Object.freeze((a => each(a, (n, k) => (a[n] = k)))([...nK].flat()))
+const is_nan = isNaN
 
-// NOTE: keys(_).length can be MUCH faster than isEmpty
-const isEmpty = x => keys(x).length == 0
-const { /*isEmpty,*/ isEqual } = _
+const is_function = x => typeof x == 'function'
+const is_boolean = x => typeof x == 'boolean'
+const is_string = x => typeof x == 'string'
+const is_object = x => typeof x == 'object'
+const is_set = x => x instanceof Set
+const is_map = x => x instanceof Map
+const is_array = Array.isArray
+const is_indexed = x =>
+  is_array(x) || (is_object(x) && Object.keys(o).every((k, j) => k == j))
+
+const empty = x => (is_object(x) ? Object.keys(x).length == 0 : _.isEmpty(x))
+const equal = _.isEqual
 const approx_equal = (x, y, ε = 0.000001) => Math.abs(y - x) < ε
