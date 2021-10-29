c57244d4dc97bce71a36949d601899290561b179
olcan <olcans@gmail.com>
2021-10-28 23:07:30 -0700

    support async tests

--- tester.js
+++ tester.js
@@ -1,12 +1,12 @@
-function test_item(item) {
+async function test_item(item) {
   if (!item.text.includes('_test')) return // no tests in item
 
   // evaluate any functions _test|_test_*() defined on item
   const tests = ['_test', ...(item.text.match(/_test_\w+/g) ?? [])]
-  tests.forEach(test => {
+  for (const test of tests) {
     try {
       const start = Date.now()
-      const tested = item.eval(
+      const tested = await item.eval(
         `typeof ${test} == 'function' ? (${test}(),true) : false`,
         { trigger: 'test', async: item.deepasync, async_simple: true }
       )
@@ -14,7 +14,7 @@ function test_item(item) {
     } catch (e) {
       item.error(`${test} failed: ${e}`)
     }
-  })
+  }
 }
 
 function _on_item_change(id, label, prev_label, deleted, remote, dependency) {
@@ -25,11 +25,11 @@ function _on_item_change(id, label, prev_label, deleted, remote, dependency) {
 }
 
 // command /test [label]
-function _on_command_test(label) {
+async function _on_command_test(label) {
   const items = _items(label)
   if (items.length == 0) {
     alert(`/test: ${label} not found`)
     return '/test ' + label
   }
-  items.forEach(test_item)
+  for (const item of items) await test_item(item)
 }
