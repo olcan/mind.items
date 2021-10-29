5945d8228a5e305da3108a0cd200aaccee3f9e05
olcan <olcans@gmail.com>
2021-10-29 12:55:06 -0700

    tests and benchmarks for is_indexed

--- util/core/types.js
+++ util/core/types.js
@@ -32,8 +32,10 @@ function _benchmark_defined() {
 }
 
 const is_integer = Number.isInteger
+const is_nan = isNaN
 const is_number = x => typeof x == 'number'
 
+// returns true iff argument is a number or a numeric string
 // from https://stackoverflow.com/a/175787
 const is_numeric = x =>
   is_number(x) || (typeof x == 'string' && !isNaN(x) && !isNaN(parseFloat(x)))
@@ -44,6 +46,8 @@ function _test_is_numeric() {
     () => is_numeric('0'),
     () => is_numeric('.0'),
     () => is_numeric('0.'),
+    () => !is_numeric(),
+    () => !is_numeric(null),
     () => !is_numeric('..0'),
     () => !is_numeric('0..'),
     () => !is_numeric('foo'),
@@ -54,25 +58,41 @@ function _test_is_numeric() {
   )
 }
 
-const is_nan = isNaN
-
 const is_function = x => typeof x == 'function'
-
 const is_boolean = x => typeof x == 'boolean'
-
 const is_string = x => typeof x == 'string'
-// const is_string = x => x?.constructor?.name == 'String'
-
 const is_object = x => typeof x == 'object'
-
 const is_set = x => x instanceof Set
-
 const is_map = x => x instanceof Map
-
 const is_array = Array.isArray
 
 const is_indexed = x =>
   is_array(x) || (is_object(x) && Object.keys(o).every((k, j) => k == j))
+
+function _test_is_indexed() {
+  check(
+    () => is_indexed([]),
+    () => is_indexed(new Array(0)),
+    () => is_indexed({}),
+    () => is_indexed(['a', 'b']),
+    () => is_indexed({ 0: 'a', 1: 'b' }),
+    () => !is_indexed(),
+    () => !is_indexed(null),
+    () => !is_indexed({ 0: 'a', b: 'b' }),
+    () => !is_indexed(new Set([0])),
+    () => !is_indexed(new Map([[0, 'a']]))
+  )
+}
+
+function _benchmark_is_indexed() {
+  benchmarks(
+    () => is_indexed([]),
+    () => is_indexed(new Array(0)),
+    () => is_indexed({}),
+    () => is_indexed({ 0: 'a', 1: 'b' }),
+    () => !is_indexed({ 0: 'a', b: 'b' })
+  )
+}
 
 const empty = x => (is_object(x) ? Object.keys(x).length == 0 : _.isEmpty(x))
 
