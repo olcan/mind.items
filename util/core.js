52ff829b00cedc1966d30eb9c6a5ed953bad2fe4
olcan <olcans@gmail.com>
2021-10-30 10:50:23 -0700

    preserve lines in comments

--- util/core.js
+++ util/core.js
@@ -146,7 +146,7 @@ function jsdoc() {
     m => {
       const def = m.groups
       def.args = def.args ?? ''
-      def.comment = def.comment?.replace(/\s+|\s*\/\/\s*/g, ' ') ?? ''
+      def.comment = def.comment?.replace(/\s*(?:\n(?:\/\/)?)\s*/g, '<br>') ?? ''
       return def
     }
   )
