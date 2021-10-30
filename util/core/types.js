e6555a6417243f0013366bd38c60f69b0feac552
Olcan <olcans@gmail.com>
2021-10-30 11:11:24 -0700

    #util/core/types:util/core/types.js edited in mind.page

--- util/core/types.js
+++ util/core/types.js
@@ -34,10 +34,10 @@ function _benchmark_defined() {
   )
 }
 
-// alias for `Number.isInteger`
+// `Number.isInteger`
 const is_integer = Number.isInteger
 
-// alias for `typeof x == 'number'`
+// `typeof x == 'number'`
 const is_number = x => typeof x == 'number'
 
 // returns true iff argument is a number or a numeric string
