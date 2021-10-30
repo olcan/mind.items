2f695aa86216ef383a0ced9712869162ff96fe58
Olcan <olcans@gmail.com>
2021-10-30 11:13:30 -0700

    #util/core/types:util/core/types.js edited in mind.page

--- util/core/types.js
+++ util/core/types.js
@@ -2,7 +2,7 @@
 const inf = Infinity
 
 // `x !== undefined`
-// `typeof x !== 'undefined'` if x is passed as string `'x'`
+// `typeof x !== 'undefined'` if `x` is passed as `"x"`
 function defined(x) {
   if (is_string(x)) return eval('typeof ' + x) !== 'undefined'
   else return x !== undefined
