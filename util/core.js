e886f0bdbec6c3658f7d2eee754653a4d2863775
olcan <olcans@gmail.com>
2021-10-30 23:44:33 -0700

    fix \s matching \n

--- util/core.js
+++ util/core.js
@@ -142,7 +142,7 @@ function jsdoc() {
     _this
       .read('js')
       .matchAll(
-        /(?:^|\n)(?<comment>(\/\/.*?\n)*)(?:function|const|let)\s+(?<name>\w+)\s*(?:(?<args>\(.*?\))|=\s*(?<arrow_args>.+?\s*=>)?\s*\n?(?<body>[^\n]+))?/gs
+        /(?:^|\n)(?<comment>(\/\/.*?\n)*)(?:function|const|let) +(?<name>\w+) *(?:(?<args>\(.*?\))|= *(?<arrow_args>.+? *=>)? *\n?(?<body>[^\n]+))?/gs
       ),
     m => {
       const def = _.merge({ args: '', comment: '' }, m.groups)
