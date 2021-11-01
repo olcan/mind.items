fb1d3fe2889331c3232bcde5dc12bcdce57ed42b
olcan <olcans@gmail.com>
2021-10-31 23:59:06 -0700

    read_input

--- util/core/item.js
+++ util/core/item.js
@@ -15,6 +15,7 @@ const read = (...args) => _this.read(...args)
 const read_deep = (...args) => _this.read_deep(...args)
 
 // read_input(type,[options])
+// reads _input_ blocks of `type` in item _+ dependencies_
 const read_input = (...args) => _this.read_input(...args)
 
 // write(text,[type],[options])
