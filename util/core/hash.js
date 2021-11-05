0df5b589bda0b0e419b64d457488bdd8e3d0e0b9
olcan <olcans@gmail.com>
2021-11-04 18:07:29 -0700

    refinements and todos for hash

--- util/core/hash.js
+++ util/core/hash.js
@@ -1,43 +1,41 @@
-function hash(x, stringifier = str) {
-  if (x?._hash) return x._hash
-  if (!defined(x)) return undefined
-  if (isFunction(x)) return hash_code('' + x)
-  if (isString(x)) return hash_code(x)
-  return hash_code(stringifier(x))
-}
+// TODO: refine these, add tests and benchmarks
 
-// from https://github.com/darkskyapp/string-hash/blob/master/index.js
-function hash_code(str) {
+function hash(x) {
+  if (!defined(x)) return undefined
+  if (is_function(x)) x = '' + x
+  if (!is_string(x)) x = JSON.stringify(x)
+  // from https://github.com/darkskyapp/string-hash/blob/master/index.js
   let hash = 5381 // see https://stackoverflow.com/q/10696223
-  let i = str.length
-  while (i) hash = (hash * 33) ^ str.charCodeAt(--i)
+  let i = x.length
+  while (i) hash = (hash * 33) ^ x.charCodeAt(--i)
   return hash >>> 0
 }
 
+function hash_sha1(x) {
+  if (!defined(x)) return undefined
+  if (is_function(x)) x = '' + x
+  if (!is_string(x)) x = JSON.stringify(x)
+  return sha1(x)
+}
+
+// TODO: use these in pusher/updater/etc
 // encodes base64 w/ unicode character support (unlike plain btoa)
 // from https://stackoverflow.com/a/30106551
-function utoa(str) {
+const encode_base64 = str =>
   // original string -> percent-encoding -> bytestream
-  return btoa(
-    encodeURIComponent(str).replace(
-      /%([0-9A-F]{2})/g,
-      function toSolidBytes(match, p1) {
-        return String.fromCharCode('0x' + p1)
-      }
+  btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (m, p1) =>
+      String.fromCharCode('0x' + p1)
     )
   )
-}
 
 // decodes base64 w/ unicode character support (unlike plain atob)
 // from https://stackoverflow.com/a/30106551
-function atou(str) {
+const decode_base64 = base64 =>
   // bytestream -> percent-encoding -> original string
-  return decodeURIComponent(
-    atob(str)
+  decodeURIComponent(
+    atob(base64)
       .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-      })
+      .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
       .join('')
   )
-}
