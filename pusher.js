f6919c7b869edbce53b69655f7f2b3575b73c9b1
olcan <olcans@gmail.com>
2021-11-05 10:43:43 -0700

    use sha1.hex in pusher/updater

--- pusher.js
+++ pusher.js
@@ -169,11 +169,11 @@ function github_sha(text) {
   const utf8 = new Uint8Array(utf8_prefix.length + utf8_text.length)
   utf8.set(utf8_prefix)
   utf8.set(utf8_text, utf8_prefix.length)
+  return sha1.hex(utf8)
   // const sha_buffer = await crypto.subtle.digest('SHA-1', utf8)
-  const sha_buffer = sha1.arrayBuffer(utf8)
-  return Array.from(new Uint8Array(sha_buffer), b =>
-    b.toString(16).padStart(2, '0')
-  ).join('')
+  // return Array.from(new Uint8Array(sha_buffer), b =>
+  //   b.toString(16).padStart(2, '0')
+  // ).join('')
 }
 
 // encodes base64 w/ unicode character support (unlike plain btoa)
