a082736a81cd309a7d762d88ec53af65514300d2
olcan <olcans@gmail.com>
2021-11-08 13:21:27 -0800

    serialize tests and benchmarks to avoid mixing up logs

--- benchmarker.js
+++ benchmarker.js
@@ -1,53 +1,59 @@
 async function benchmark_item(item) {
   if (!item.text.match(/\b_benchmark/)) return // no benchmarks in item
 
-  // evaluate any functions _benchmark|_benchmark_*() defined on item
-  const benchmarks = [
-    '_benchmark',
-    ...(item.text.match(/\b_benchmark_\w+/g) ?? []),
-  ]
-  for (const benchmark of benchmarks) {
-    const name = benchmark.replace(/^_benchmark_?/, '') || '(unnamed)'
-    let done, ms, e
-    const start = Date.now()
-    try {
-      done = await item.eval(
-        `typeof ${benchmark} == 'function' ? (${benchmark}(),true) : false`,
-        { trigger: 'benchmark', async: item.deepasync, async_simple: true }
-      )
-      ms = Date.now() - start
-      if (done) item.log(`BENCHMARK ${name} completed in ${ms}ms`)
-    } catch (_e) {
-      done = true // since error thrown
-      ms = Date.now() - start
-      item.error(`BENCHMARK ${name} FAILED in ${ms}ms; ${_e}`)
-      e = _e
-    }
-    // store benchmark results in item's global store under _tests
-    if (done) {
-      const log = item.get_log({ since: 'eval' })
-      const gs = item.global_store
-      gs._benchmarks = _.set(gs._benchmarks || {}, name, { ms, ok: !e, log })
-      // look up benchmarked function names
-      let names
+  // serialize via item.store._benchmarker/_tester to avoid mixing up logs
+  item.store._benchmarker = Promise.allSettled([
+    item.store._benchmarker,
+    item.store._tester,
+  ]).then(async () => {
+    // evaluate any functions _benchmark|_benchmark_*() defined on item
+    const benchmarks = [
+      '_benchmark',
+      ...(item.text.match(/\b_benchmark_\w+/g) ?? []),
+    ]
+    for (const benchmark of benchmarks) {
+      const name = benchmark.replace(/^_benchmark_?/, '') || '(unnamed)'
+      let done, ms, e
+      const start = Date.now()
       try {
-        names = await item.eval(
-          `typeof ${benchmark}_functions == 'object' ? ` +
-            `${benchmark}_functions : null`
+        done = await item.eval(
+          `typeof ${benchmark} == 'function' ? (${benchmark}(),true) : false`,
+          { trigger: 'benchmark', async: item.deepasync, async_simple: true }
         )
-      } catch (e) {}
-      if (is_array(names) && names.every(is_string)) {
-        names.forEach(name => {
-          gs._benchmarks = _.set(gs._benchmarks || {}, name, {
-            ms,
-            ok: !e,
-            log,
-          })
-        })
+        ms = Date.now() - start
+        if (done) item.log(`BENCHMARK ${name} completed in ${ms}ms`)
+      } catch (_e) {
+        done = true // since error thrown
+        ms = Date.now() - start
+        item.error(`BENCHMARK ${name} FAILED in ${ms}ms; ${_e}`)
+        e = _e
       }
-      await _delay(1) // ensure time-separation of benchmark runs (and logs)
+      // store benchmark results in item's global store under _tests
+      if (done) {
+        const log = item.get_log({ since: 'eval' })
+        const gs = item.global_store
+        gs._benchmarks = _.set(gs._benchmarks || {}, name, { ms, ok: !e, log })
+        // look up benchmarked function names
+        let names
+        try {
+          names = await item.eval(
+            `typeof ${benchmark}_functions == 'object' ? ` +
+              `${benchmark}_functions : null`
+          )
+        } catch (e) {}
+        if (is_array(names) && names.every(is_string)) {
+          names.forEach(name => {
+            gs._benchmarks = _.set(gs._benchmarks || {}, name, {
+              ms,
+              ok: !e,
+              log,
+            })
+          })
+        }
+        await _delay(1) // ensure time-separation of benchmark runs (and logs)
+      }
     }
-  }
+  })
 }
 
 function _on_item_change(id, label, prev_label, deleted, remote, dependency) {
--- tester.js
+++ tester.js
@@ -1,49 +1,56 @@
 async function test_item(item) {
   if (!item.text.match(/\b_test/)) return // no tests in item
 
-  // evaluate any functions _test|_test_*() defined on item
-  const tests = ['_test', ...(item.text.match(/\b_test_\w+/g) ?? [])]
-  for (const test of tests) {
-    const name = test.replace(/^_test_?/, '') || '(unnamed)'
-    let done, ms, e
-    const start = Date.now()
-    try {
-      done = await item.eval(
-        `typeof ${test} == 'function' ? (${test}(),true) : false`,
-        { trigger: 'test', async: item.deepasync, async_simple: true }
-      )
-      ms = Date.now() - start
-      if (done) item.log(`TEST ${name} passed in ${ms}ms`)
-    } catch (_e) {
-      done = true // since error thrown
-      ms = Date.now() - start
-      item.error(`TEST ${name} FAILED in ${ms}ms; ${_e}`)
-      e = _e
-    }
-    // store test results in item's global store under _tests
-    if (done) {
-      const log = item.get_log({ since: 'eval' })
-      const gs = item.global_store
-      gs._tests = _.set(gs._tests || {}, name, { ms, ok: !e, log })
-      // look up tested function names
-      let names
+  // serialize via item.store._tester/_benchmarker to avoid mixing up logs
+  item.store._tester = Promise.allSettled([
+    item.store._tester,
+    item.store._benchmarker,
+  ]).then(async () => {
+    // evaluate any functions _test|_test_*() defined on item
+    const tests = ['_test', ...(item.text.match(/\b_test_\w+/g) ?? [])]
+    for (const test of tests) {
+      const name = test.replace(/^_test_?/, '') || '(unnamed)'
+      let done, ms, e
+      const start = Date.now()
       try {
-        names = await item.eval(
-          `typeof ${test}_functions == 'object' ? ` + `${test}_functions : null`
+        done = await item.eval(
+          `typeof ${test} == 'function' ? (${test}(),true) : false`,
+          { trigger: 'test', async: item.deepasync, async_simple: true }
         )
-      } catch (e) {}
-      if (is_array(names) && names.every(is_string)) {
-        names.forEach(name => {
-          gs._tests = _.set(gs._tests || {}, name, {
-            ms,
-            ok: !e,
-            log,
-          })
-        })
+        ms = Date.now() - start
+        if (done) item.log(`TEST ${name} passed in ${ms}ms`)
+      } catch (_e) {
+        done = true // since error thrown
+        ms = Date.now() - start
+        item.error(`TEST ${name} FAILED in ${ms}ms; ${_e}`)
+        e = _e
       }
-      await _delay(1) // ensure time-separation of test runs (and logs)
+      // store test results in item's global store under _tests
+      if (done) {
+        const log = item.get_log({ since: 'eval' })
+        const gs = item.global_store
+        gs._tests = _.set(gs._tests || {}, name, { ms, ok: !e, log })
+        // look up tested function names
+        let names
+        try {
+          names = await item.eval(
+            `typeof ${test}_functions == 'object' ? ` +
+              `${test}_functions : null`
+          )
+        } catch (e) {}
+        if (is_array(names) && names.every(is_string)) {
+          names.forEach(name => {
+            gs._tests = _.set(gs._tests || {}, name, {
+              ms,
+              ok: !e,
+              log,
+            })
+          })
+        }
+        await _delay(1) // ensure time-separation of test runs (and logs)
+      }
     }
-  }
+  })
 }
 
 function _on_item_change(id, label, prev_label, deleted, remote, dependency) {
