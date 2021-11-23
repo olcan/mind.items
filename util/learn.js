3b39b292feda2fdfb29a634c127ce9e306e7e86c
olcan <olcans@gmail.com>
2021-11-23 03:52:21 -0800

    refined learn doc

--- util/learn.js
+++ util/learn.js
@@ -11,10 +11,12 @@ function _run() {
 // _learned_ value from `domain`
 // | string | type string, `≡{is:'type'}`
 // | array  | value array, `≡{in:[v0,v1,…]}`
-// | `{eq|gte|lte|gt|lt:value}` | numeric set
-// | `{is:type}` | type, e.g. `{is:'number'}`
-// | `{in:[v0,v1,…]}` | possible values
-// | `{and|or:[{…},…]}` | composite domain
+// | object | domain spec w/ fields:
+// | `is:type` | type string, e.g. `'boolean'`, `'number'`, …
+// | `in:[v0,v1,…]` | possible values `x==v0`, `x==v1`, …
+// | `gte|lte:value` | inequality `x≥value`, `x≤value`
+// | `gt|lt:value`   | strict inequality `x>value`, `x<value`
+// | `and|or:[{…},…]` | composite domain
 // requires feedback via `need(…)` or `want(…)`
 function learn(domain = { type: 'boolean' }, options = {}) {
   const { name } = options
