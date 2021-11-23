780733c7c41c45a49b7985f0a0f64d09e2049a72
olcan <olcans@gmail.com>
2021-11-22 16:12:25 -0800

    progress on util/learn

--- util/learn.js
+++ util/learn.js
@@ -2,25 +2,24 @@ function _run() {
   const js = _this.read('js_input')
   if (js.match(/\b(?:learn|need|want)\(.*\)/s)) {
     log('run handled by #util/learn')
-    // TODO:
     // benchmark(() => eval(js))
     return eval(js)
   }
   return null // skip
 }
 
-// learned value out of `domain`
-// `domain` can be an array of possible values
-// `domain` can be a comparison object `{eq, gte, gt, lt, lte}`
-// `domain` can be an array of comparison objects
+// returns _learned_ value from `domain`
+// `domain` can be a type string, e.g. `'boolean'`
+// `domain` can be an array of possible values, e.g. `[1,5,10]`
+// `domain` can be a comparison object `{eq, gte, lte, gt, lt, and, or, type}`
 // requires feedback via `need(…)` or `want(…)`
-// feedback can be provided in the future
-function learn(domain, options = {}) {
+// feedback can be delayed
+function learn(domain = 'boolean', options = {}) {
   const { name = stringify(f) } = options
 }
 
-// todo
-function need() {}
+// declares preference for `cond` (to be true)
+function want(cond, [penalty = -1]) {}
 
-// todo
-function want() {}
+// declares requirement `≡ want(cond, -inf)`
+const need = cond => want(cond, -inf)
