465d3ad3184bf2d47f596423c74bb01d2283b9d5
olcan <olcans@gmail.com>
2022-01-02 12:18:16 -0800

    normal sampler, and comments for upcoming gamma sampler

--- util/sample/domains.js
+++ util/sample/domains.js
@@ -1,17 +1,17 @@
 // interval domain `(a,b)`
-// can be _unbounded_ (`±inf`) on either sider
+// can be _unbounded_ (`±inf`) on either side
 // sampler depends on (may require) additional `options`:
-// | `mean` (`μ`)  | prior mean
-// | `mode` (`c`)  | prior mode (alternative to `mean`)
-// | `stdev` (`σ`) | prior standard deviation
+// | `mode` or `c`  | prior mode
+// | `mean` or `μ`  | prior mean
+// | `stdev` or `σ` | prior standard deviation
 // \
 // | **domain** | **options** | **sampler**
 // | `(a,b)`      |             | `uniform(a,b)`
 // | `(a,b)`      | `c`         | `triangular(a,b,c)`
 // | `(a,b)`      | `μ`,`σ`     | `beta(a,b,μ,σ)`
-// | `(a,+∞)`     | `μ|c`, `σ`  | `transform(gamma(μ,σ), x=> a+x)`
-// | `(-∞,b)`     | `μ|c`, `σ`  | `transform(gamma(μ,σ), x=> b-x)`
-// | `(-∞,+∞)`    | `μ|c`, `σ`  | `normal(μ,σ)`
+// | `(a,∞)`      | `μ|c`, `σ`  | `gamma(a,μ,σ)`
+// | `(-∞,b)`     | `μ|c`, `σ`  | `gamma(b,μ,-σ)`
+// | `(-∞,∞)`     | `μ|c`, `σ`  | `normal(μ,σ)`
 // `undefined` if `a` or `b` non-number
 // `null` (empty) if `a>=b`
 const interval = (a, b, options = undefined) => {
@@ -19,10 +19,25 @@ const interval = (a, b, options = undefined) => {
   if (!is_number(a) || !is_number(b)) return undefined
   // empty (null) if a >= b
   if (a >= b) return null
-  // handle bounded interval: uniform or triangular if mode (c) specified
+  // bounded interval is uniform or triangular if mode (c) specified
   if (is_finite(a) && is_finite(b)) {
-    if (defined(options?.mode)) return triangular(a, b, options.mode)
+    let c
+    if (options) c = options.mode ?? options.c
+    if (defined(c)) return triangular(a, b, c)
     return uniform(a, b)
   }
+  // unbounded interval is normal w/ `μ|c` and `σ` required
+  if (is_inf(a) && is_inf(b)) {
+    let μ, σ
+    if (options) {
+      μ = options.mean ?? options.μ ?? options.mode ?? options.c
+      σ = options.stdev ?? options.σ
+    }
+    assert(defined(μ), 'missing mean or mode for unbounded interval (-∞,∞)')
+    assert(defined(σ), 'missing stdev for unbounded interval (-∞,∞)')
+    return normal(μ, σ)
+  }
+  // half-unbounded interval is gamma w/ `μ|c` and `σ` required
+  // TODO: implement gamma(a,μ,±σ) sampler
   fatal('not yet implemented')
 }
