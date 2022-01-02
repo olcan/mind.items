f0f9146e5e48a342d71df62d37f0e2b913e7ce89
olcan <olcans@gmail.com>
2022-01-02 09:56:13 -0800

    added triangular sampler and refined example 9

--- util/sample/domains.js
+++ util/sample/domains.js
@@ -4,22 +4,25 @@
 // | `mean` (`μ`)  | prior mean
 // | `mode` (`c`)  | prior mode (alternative to `mean`)
 // | `stdev` (`σ`) | prior standard deviation
-// undefined if `a` or `b` non-number
-// null (empty) if `a>=b`
-// | **interval** | **options** | **sampler**
-// | `(a,b)`      |             | `transform(uniform(a,b), x=>x>a?x:undefined)`
-// | `(a,b)`      | `c|μ`       | `transform(triangular(a,b,c), x=>x>a&&x<b?x:undefined)`
+// \
+// | **domain** | **options** | **sampler**
+// | `(a,b)`      |             | `uniform(a,b)`
+// | `(a,b)`      | `c`         | `triangular(a,b,c)`
+// | `(a,b)`      | `μ`,`σ`     | `beta(a,b,μ,σ)`
 // | `(a,+∞)`     | `μ|c`, `σ`  | `transform(gamma(μ,σ), x=> a+x)`
 // | `(-∞,b)`     | `μ|c`, `σ`  | `transform(gamma(μ,σ), x=> b-x)`
 // | `(-∞,+∞)`    | `μ|c`, `σ`  | `normal(μ,σ)`
+// undefined if `a` or `b` non-number
+// null (empty) if `a>=b`
 const interval = (a, b, options = undefined) => {
-  // undefined for non-number args
+  // undefined if a or b non-number
   if (!is_number(a) || !is_number(b)) return undefined
-  // TODO: fix these, checking and converting options as needed
-  const finite_a = is_finite(a)
-  const finite_b = is_finite(b)
-  if (finite_a && finite_b) return uniform(a, b)
-  if (finite_a) return transform(x => a + x, { x: gamma(μ, σ) })
-  if (finite_b) return transform(x => b - x, { x: gamma(μ, σ) })
-  return normal(μ, σ)
+  // empty (null) if a >= b
+  if (a >= b) return null
+  // handle bounded interval: uniform or triangular if mode (c) specified
+  if (is_finite(a) && is_finite(b)) {
+    if (defined(options?.mode)) return triangular(a, b, options.mode)
+    return uniform(a, b)
+  }
+  fatal('not yet implemented')
 }
--- util/sample/samplers.js
+++ util/sample/samplers.js
@@ -1,37 +1,65 @@
+// posterior sampler that samples uniformly in (x-r,x+r) w/ r=stdev
+// clips and shifts region inside (a,b) to avoid asymmetry
+// can be used for any bounded domain or prior sampler
+const _uniform_posterior = (a, b) => (f, x, stdev) => {
+  if (stdev == 0) return f(a + (b - a) * random()) // degenerate sample
+  const r = min((b - a) / 2, stdev)
+  let ya = x - r
+  let yb = x + r
+  if (ya < a) (yb += a - ya), (ya = a)
+  else if (yb > b) (ya -= yb - b), (yb = b)
+  return f(ya + (yb - ya) * random())
+}
+
 // uniform(a,b)
 // uniform sampler on `(a,b)`
 // undefined if `a` or `b` non-number or infinite
 // null (empty) if `a>=b`
 function uniform(a, b) {
-  // undefined if non-number or infinite args
+  // undefined if a or b non-number or infinite
   if (!is_number(a) || is_inf(a)) return undefined
   if (!is_number(b) || is_inf(b)) return undefined
   // empty (null) if a >= b
   if (a >= b) return null
   return {
-    gte: a,
+    gt: a,
     lt: b,
-    _log_p: x => (x >= a && x < b ? -log(b - a) : -inf),
+    _log_p: x => (x > a && x < b ? -log(b - a) : -inf),
     _prior: f => f(a + (b - a) * random()),
-    _posterior: (f, x, stdev) => {
-      const u = random()
-      // stdev can be 0 w/ single positive-weight point
-      if (stdev == 0) return f(a + (b - a) * u)
-      // sample around x w/ random radius proportional to stdev
-      // shift sampling region inside (a,b) to avoid asymmetry in x<->y
-      const r = min((b - a) / 2, stdev)
-      let ya = x - r
-      let yb = x + r
-      if (ya < a) (yb += a - ya), (ya = a)
-      else if (yb > b) (ya -= yb - b), (yb = b)
-      return f(ya + (yb - ya) * u)
-    },
+    _posterior: _uniform_posterior(a, b),
   }
 }
 
-// TODO: drop "transform" for now, instead make uniform and triangular open-domain and gamma w/ translation/negation support, and normal; implement and benchmark positive_random
-
-// TODO: new samplers: transform, gamma, normal, ...
+// triangular(a,b,c)
+// triangular sampler on `(a,b)` w/ mode `c`
+// undefined if `a` or `b` non-number or infinite
+// undefined if `c` non-number or `c∉[a,b]`
+// null (empty) if `a>=b`
+function triangular(a, b, c) {
+  // undefined if a or b non-number or infinite
+  if (!is_number(a) || is_inf(a)) return undefined
+  if (!is_number(b) || is_inf(b)) return undefined
+  if (!is_number(c) || c < a || c > b) return undefined
+  // empty (null) if a >= b
+  if (a >= b) return null
+  return {
+    gt: a,
+    lt: b,
+    _log_p: x => {
+      if (x <= a) return -inf
+      if (x < c) return log(2) + log(x - a) - log(b - a) - log(c - a)
+      if (x == c) return log(2) - log(b - a)
+      if (x < b) return log(2) + log(b - x) - log(b - a) - log(b - c)
+      return -inf
+    },
+    _prior: f => {
+      const u = random()
+      if (u < (c - a) / (b - a)) return f(a + Math.sqrt(u * (b - a) * (c - a)))
+      return f(b - Math.sqrt((1 - u) * (b - a) * (b - c)))
+    },
+    _posterior: _uniform_posterior(a, b),
+  }
+}
 
 function _benchmark_uniform() {
   benchmark(() => uniform(0, 1))
