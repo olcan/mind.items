a1afbe1f40bfc6aad83723f3bd9e2797ebeb9672
olcan <olcans@gmail.com>
2022-01-28 15:32:26 -0800

    tweaks to example 12 and sim

--- util/sim.js
+++ util/sim.js
@@ -104,10 +104,12 @@ const _if = (fc, ft, fx, fθ) => _event(fx, ft, fc, fθ)
 // new simulation must also start from new state
 const reset = (...events) => each(flat(events), e => delete e.t)
 
-// create state with `props` at time `t`
+// create state with `props`
 // `props` can be any [cloneable](https://lodash.com/docs/4.17.15#clone) object
+// `props` can include time `t`; default starting time is `t=0`
 // `history` can be `0` (none), `1` (events), or `2` (events & states)
-const state = (props, t = 0, history = 0) => {
+const state = (props, history = 0) => {
+  const { t = 0 } = props
   assert(is_finite(t), `invalid state time ${t}`)
   switch (history) {
     case 0:
