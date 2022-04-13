0eb73600e65e3dcd6c3422e6c7525a0a1dac0280
olcan <olcans@gmail.com>
2022-04-13 12:58:22 -0700

    init_worker and eval_on_worker in util/core

--- util/core.js
+++ util/core.js
@@ -52,12 +52,14 @@ const map_keys = _.mapKeys
 const map_values = _.mapValues
 
 // default-numeric sort/rank functions
+
 const sort = (xJ, f = (a, b) => a - b) => xJ.sort(f)
 const sort_by = (xJ, f = x => x) => xJ.sort((a, b) => f(a) - f(b))
 const rank = (xJ, f = (a, b) => a - b) => xJ.sort((a, b) => f(b, a))
 const rank_by = (xJ, f = x => x) => xJ.sort((a, b) => f(b) - f(a))
 
 // other useful comparator functions
+
 const compare = (a, b) => (a < b ? -1 : a > b ? 1 : 0)
 const compare_locale = (a, b, ...args) => a.localeCompare(b, ...args)
 
@@ -173,7 +175,7 @@ function parse(text) {
   })
 }
 
-// converts `x` to a simple string
+// convert `x` to a simple string
 // mainly intended for logging, output, etc
 // not intended for parsing, unlike e.g. `JSON.stringify`
 // | string   | `x` wrapped in single quotes
@@ -258,7 +260,7 @@ function _test_str() {
   )
 }
 
-// rounds `x` to `d` decimal places
+// round `x` to `d` decimal places
 // `d` can be negative for digits _before_ decimal point
 // `d` can be restricted to at most `s` significant (non-zero) digits
 // `mode` string can be `round`, `floor`, or `ceil`
@@ -378,7 +380,7 @@ function _significant_digits(x) {
   return t2.replace(/0+$/, '').length
 }
 
-// throws error if any of `funcs` returns falsy
+// throw error if any of `funcs` returns falsy
 // if array is returned, all elements must be `equal`
 // typically used in test functions `_test_*()`
 function check(...funcs) {
@@ -398,7 +400,7 @@ function check(...funcs) {
   })
 }
 
-// measures calls/sec for each of `funcs`
+// measure calls/sec for each of `funcs`
 // typically used in benchmark functions `_benchmark_*()`
 // uses shuffle & silent pass to reduce & randomize ordering bias
 function benchmark(...funcs) {
@@ -557,6 +559,124 @@ function cache(obj, prop, deps, f, options = {}) {
     ...options,
   })
   obj['_' + prop] = null // init as null
+}
+
+// initialize worker
+// `options.imports` can be (array of) `/path` or `#item` strings
+// default imports are lodash (`/lodash.min.js`) and `#util/core`
+function init_worker(options = {}) {
+  const { imports = ['/lodash.min.js', '#util/core'] } = options
+  // create worker
+  // const worker = new Worker('worker.js')
+  const host = location.protocol + '//' + location.host
+  const js = [
+    imports.filter(s => s[0] == '/').map(s => `import '${host}/lodash.min.js'`),
+    imports.filter(s => s[0] == '#').map(s => _item(s)?.read_deep('js')),
+    `self.onmessage = function(e){ self.onmessage=null; eval(e.data) }`,
+  ]
+    .flat()
+    .filter(s => s)
+    .join(';\n')
+  const worker = new Worker(
+    URL.createObjectURL(new Blob([js], { type: 'application/javascript' })),
+    { type: 'module' }
+  ) // module works support import keyword
+  worker.id = _hash(Date.now()) // unique id based on init time
+  worker.item = _this // creator item
+  worker.host = host // host url for imports
+  // set up message handler to invoke item methods or log to console
+  worker.onmessage = e => {
+    // handle item method calls from worker
+    if (e.data.item && e.data.method) {
+      // invoke method on item
+      // note if any errors are thrown, call stack will not be helpful
+      // corresponding worker-side errors should provide much more context
+      return _item(e.data.item)?.[e.data.method]?.(...(e.data.args ?? []))
+    }
+    // log all other messages to console as warning
+    warn(
+      `non-item message from worker ${worker.id} (${worker.item.name}): ` +
+        e.data
+    )
+    // write_log()
+  }
+  // initialize worker via initial eval
+  function init(item, host) {
+    const start = Date.now()
+
+    // set up _this to redirect (most) _Item methods to initializing item
+    // see #util/item for listing of _Item methods
+    self._this = new Proxy(
+      {},
+      {
+        get(target, method) {
+          switch (method) {
+            case 'constructor':
+              return null // so _Item = null in #util/item
+            case 'eval':
+            case 'write':
+            case 'write_lines':
+            case 'clear':
+            case 'remove':
+            case 'delete':
+            case 'write_log':
+            case 'write_log_any':
+            case 'show_logs':
+            case 'touch':
+            case 'save':
+            case 'debug':
+            case 'print':
+            case 'info':
+            case 'warn':
+            case 'error':
+              return (...args) => postMessage({ item, method, args })
+            case 'fatal':
+              return (...args) => {
+                postMessage({ item, method, args })
+                throw new Error('stopping worker eval at fatal error')
+              }
+            default:
+              console.error(`_Item.${method} not available in worker`)
+          }
+        },
+      }
+    )
+
+    // set up message handler for custom js eval via message {js:'...'}
+    self.onmessage = e => {
+      const { js, context } = e.data
+      if (js) {
+        if (context) eval(`(function({${_.keys(context)}}){${js}})`)(context)
+        else eval(js)
+      }
+    }
+
+    // load lodash as _ (not for module workers, which should use import keyword)
+    // importScripts(host + '/lodash.min.js')
+    // _this.print(`loaded lodash (${_?.VERSION}) in ${Date.now()-start}ms`)
+
+    _this.print(`initialized worker in ${Date.now() - start}ms`)
+  }
+  worker.postMessage(`(${init})('${_this.name}','${host}')`)
+
+  return worker
+}
+
+// evaluate `js` on `worker`
+// `js` can be string or function
+// | `deps`     | optional dependency item names
+// | `context`  | optional context object (can contain transferables)
+// | `transfer` | optional transferables (e.g. typed array buffers)
+function eval_on_worker(worker, js, options = {}) {
+  let { deps, context, transfer } = options
+  if (deps) deps = [deps].flat()
+  if (transfer) transfer = [transfer].flat()
+  if (typeof js == 'function') js = `(${js})()`
+  if (!is_string(js)) fatal(`invalid argument js; must be function or string`)
+  js = [deps?.map(s => _item(s)?.read_deep('js')), js]
+    .filter(s => s)
+    .join(';\n')
+  return worker.postMessage({ js, context }, transfer)
 }
 
 // markdown table for `cells`
