7234fa582919f65cbb2fd4b4086abcd633351f1e
Olcan <olcans@gmail.com>
2022-10-21 13:56:44 -0700

    #sharer:sharer.js (via #pusher)

--- sharer.js
+++ sharer.js
@@ -1,1 +1,7 @@
-// TODO
+function _on_welcome() {
+  // find all items w/ tags #_shared/[\w-]+(?:\/\d+)
+  // ensure consistency of tags w/ attr, including deps ...
+  // share based on tag (w/ optional index), including all dependents (no index)
+  // keep track of all sharing keys for all items (including dependencies)
+  // remove keys not backed by any tags (on self or dependent)
+}
